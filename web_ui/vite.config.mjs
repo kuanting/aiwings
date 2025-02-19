581ce8a47130dabedf0284983210facf27301bae
Riley Ho <riley@rileychh.dev>
2025-02-19 21:23:44 +0800

    feat(web): migrate to Vite 6

--- web_ui/vite.config.mjs
+++ web_ui/vite.config.mjs
@@ -11,12 +11,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler'
-      }
-    }
   }
 })
